34dd1ff089969d3111ec1a4687672bd11059c8f0
Yana Ushakova <junk7day@gmail.com>
2023-02-28 23:53:48 +0300

    add id

--- app/src/main/java/com/example/giphy/data/network/model/GifDto.kt
+++ app/src/main/java/com/example/giphy/data/network/model/GifDto.kt
@@ -6,5 +6,6 @@ data class GifDto(
     @SerializedName("images") val imageDto: ImageDto,
     @SerializedName("type") val type: String?,
     @SerializedName("rating") val rating: String?,
-    @SerializedName("url") val giphyUrl: String?
+    @SerializedName("url") val giphyUrl: String?,
+    @SerializedName("id") val id: String?
 )
