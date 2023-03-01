12b0508b10abec9dff9745bf595615775e123260
Yana Ushakova <junk7day@gmail.com>
2023-03-01 16:39:40 +0300

    use search function

--- app/src/main/java/com/example/giphy/presentation/GifViewModel.kt
+++ app/src/main/java/com/example/giphy/presentation/GifViewModel.kt
@@ -6,18 +6,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.giphy.domain.entity.Gif
 import com.example.giphy.domain.usecases.GetGifListUseCase
+import com.example.giphy.domain.usecases.SearchGifListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GifViewModel @Inject constructor(
-    private val getGifListUseCase: GetGifListUseCase
+    private val getGifListUseCase: GetGifListUseCase,
+    private val searchGifListUseCase: SearchGifListUseCase
 ) : ViewModel() {
 
     private val _gifList = MutableLiveData<List<Gif>>()
     val gifList: LiveData<List<Gif>>
         get() = _gifList
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
 
     init {
         getGifList()
@@ -25,8 +32,21 @@ class GifViewModel @Inject constructor(
 
     private fun getGifList() {
         viewModelScope.launch {
-            getGifListUseCase.invoke().let {
+            getGifListUseCase().let {
                 _gifList.value = it
+            }
+        }
+    }
+
+    fun searchGifList(str: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            delay(500L)
+            searchGifListUseCase(str).let {
+                if (checkNotNull(it).isNotEmpty()) {
+                    _gifList.value = it
+                }
+                _isLoading.value = false
             }
         }
     }
