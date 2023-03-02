7bc495e16184ed304219a122cb9ba9e9bd5fad02
Yana Ushakova <junk7day@gmail.com>
2023-03-02 16:47:07 +0300

    use parcelize

--- app/src/main/java/com/example/giphy/domain/entity/Gif.kt
+++ app/src/main/java/com/example/giphy/domain/entity/Gif.kt
@@ -1,11 +1,14 @@
 package com.example.giphy.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import javax.inject.Inject
 
+@Parcelize
 data class Gif @Inject constructor(
     val imageUrl: String?,
     val type: String?,
     val rating: String?,
     val giphyUrl: String?,
     val id: String?
-)
+): Parcelable
